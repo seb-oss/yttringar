a1f705d4a3faad7cbda4c1b7ab01055b24004d5f
Gråborg <mikael.graborg@seb.se>
2019-11-18 10:54:04 +0100

    chore: prettify and remove unnecessary functionality

--- src/utterances.ts
+++ src/utterances.ts
@@ -1,148 +1,155 @@
-import { pageAttributes as page } from './page-attributes';
+import { pageAttributes as page } from './page-attributes'
 import {
   Issue,
   setRepoContext,
   loadIssueByTerm,
-  loadIssueByNumber,
   loadCommentsPage,
   loadUser,
   postComment,
   createIssue,
   PAGE_SIZE,
   IssueComment
-} from './github';
-import { TimelineComponent } from './timeline-component';
-import { NewCommentComponent } from './new-comment-component';
-import { startMeasuring, scheduleMeasure } from './measure';
-import { loadTheme } from './theme';
-import { getRepoConfig } from './repo-config';
-import { loadToken } from './oauth';
-import { enableReactions } from './reactions';
+} from './github'
+import { TimelineComponent } from './timeline-component'
+import { NewCommentComponent } from './new-comment-component'
+import { startMeasuring, scheduleMeasure } from './measure'
+import { loadTheme } from './theme'
+import { getRepoConfig } from './repo-config'
+import { loadToken } from './oauth'
+import { enableReactions } from './reactions'
 
-setRepoContext(page);
+setRepoContext(page)
 
 function loadIssue(): Promise<Issue | null> {
-  if (page.issueNumber !== null) {
-    return loadIssueByNumber(page.issueNumber);
-  }
-  return loadIssueByTerm(page.issueTerm as string);
+  return loadIssueByTerm(page.issueTerm as string)
 }
 
 async function bootstrap() {
-  await loadToken();
+  await loadToken()
   // tslint:disable-next-line:prefer-const
   let [issue, user] = await Promise.all([
     loadIssue(),
     loadUser(),
     loadTheme(page.theme, page.origin)
-  ]);
+  ])
 
-  startMeasuring(page.origin);
+  startMeasuring(page.origin)
 
-  const timeline = new TimelineComponent(user, issue);
-  document.body.appendChild(timeline.element);
+  const timeline = new TimelineComponent(user, issue)
+  document.body.appendChild(timeline.element)
 
   if (issue && issue.comments > 0) {
-    renderComments(issue, timeline);
+    renderComments(issue, timeline)
   }
 
-  scheduleMeasure();
+  scheduleMeasure()
 
   if (issue && issue.locked) {
-    return;
+    return
   }
 
-  enableReactions(!!user);
+  enableReactions(!!user)
 
   const submit = async (markdown: string) => {
-    await assertOrigin();
+    await assertOrigin()
+
     if (!issue) {
       issue = await createIssue(
         page.issueTerm as string,
         page.url,
         page.title,
-        page.description || '',
+        markdown,
         page.label
-      );
-      timeline.setIssue(issue);
+      )
+      timeline.setIssue(issue)
     }
-    const comment = await postComment(issue.number, markdown);
-    timeline.insertComment(comment, true);
-    newCommentComponent.clear();
-  };
 
-  const newCommentComponent = new NewCommentComponent(user, submit);
-  timeline.element.appendChild(newCommentComponent.element);
+    const comment = await postComment(issue.number, markdown)
+    timeline.insertComment(comment, true)
+    newCommentComponent.clear()
+  }
+
+  const newCommentComponent = new NewCommentComponent(user, submit)
+  timeline.element.appendChild(newCommentComponent.element)
 }
 
-bootstrap();
+bootstrap()
 
 addEventListener('not-installed', function handleNotInstalled() {
-  removeEventListener('not-installed', handleNotInstalled);
-  document.querySelector('.timeline')!.insertAdjacentHTML('afterbegin', `
+  removeEventListener('not-installed', handleNotInstalled)
+  document.querySelector('.timeline')!.insertAdjacentHTML(
+    'afterbegin',
+    `
   <div class="flash flash-error">
     Error: utterances is not installed on <code>${page.owner}/${page.repo}</code>.
     If you own this repo,
     <a href="https://github.com/apps/utterances" target="_top"><strong>install the app</strong></a>.
     Read more about this change in
     <a href="https://github.com/utterance/utterances/pull/25" target="_top">the PR</a>.
-  </div>`);
-  scheduleMeasure();
-});
+  </div>`
+  )
+  scheduleMeasure()
+})
 
 async function renderComments(issue: Issue, timeline: TimelineComponent) {
   const renderPage = (page: IssueComment[]) => {
     for (const comment of page) {
-      timeline.insertComment(comment, false);
+      timeline.insertComment(comment, false)
     }
-  };
+  }
 
-  const pageCount = Math.ceil(issue.comments / PAGE_SIZE);
+  const pageCount = Math.ceil(issue.comments / PAGE_SIZE)
   // always load the first page.
-  const pageLoads = [loadCommentsPage(issue.number, 1)];
+  const pageLoads = [loadCommentsPage(issue.number, 1)]
   // if there are multiple pages, load the last page.
   if (pageCount > 1) {
-    pageLoads.push(loadCommentsPage(issue.number, pageCount));
+    pageLoads.push(loadCommentsPage(issue.number, pageCount))
   }
   // if the last page is small, load the penultimate page.
   if (pageCount > 2 && issue.comments % PAGE_SIZE < 3) {
-    pageLoads.push(loadCommentsPage(issue.number, pageCount - 1));
+    pageLoads.push(loadCommentsPage(issue.number, pageCount - 1))
   }
   // await all loads to reduce jank.
-  const pages = await Promise.all(pageLoads);
+  const pages = await Promise.all(pageLoads)
   for (const page of pages) {
-    renderPage(page);
+    renderPage(page)
   }
   // enable loading hidden pages.
-  let hiddenPageCount = pageCount - pageLoads.length;
-  let nextHiddenPage = 2;
+  let hiddenPageCount = pageCount - pageLoads.length
+  let nextHiddenPage = 2
   const renderLoader = (afterPage: IssueComment[]) => {
     if (hiddenPageCount === 0) {
-      return;
+      return
     }
     const load = async () => {
-      loader.setBusy();
-      const page = await loadCommentsPage(issue.number, nextHiddenPage);
-      loader.remove();
-      renderPage(page);
-      hiddenPageCount--;
-      nextHiddenPage++;
-      renderLoader(page);
-    };
-    const afterComment = afterPage.pop()!;
-    const loader = timeline.insertPageLoader(afterComment, hiddenPageCount * PAGE_SIZE, load);
-  };
-  renderLoader(pages[0]);
+      loader.setBusy()
+      const page = await loadCommentsPage(issue.number, nextHiddenPage)
+      loader.remove()
+      renderPage(page)
+      hiddenPageCount--
+      nextHiddenPage++
+      renderLoader(page)
+    }
+    const afterComment = afterPage.pop()!
+    const loader = timeline.insertPageLoader(
+      afterComment,
+      hiddenPageCount * PAGE_SIZE,
+      load
+    )
+  }
+  renderLoader(pages[0])
 }
 
 export async function assertOrigin() {
-  const { origins } = await getRepoConfig();
-  const { origin, owner, repo } = page;
+  const { origins } = await getRepoConfig()
+  const { origin, owner, repo } = page
   if (origins.indexOf(origin) !== -1) {
-    return;
+    return
   }
 
-  document.querySelector('.timeline')!.lastElementChild!.insertAdjacentHTML('beforebegin', `
+  document.querySelector('.timeline')!.lastElementChild!.insertAdjacentHTML(
+    'beforebegin',
+    `
   <div class="flash flash-error flash-not-installed">
     Error: <code>${origin}</code> is not permitted to post to <code>${owner}/${repo}</code>.
     Confirm this is the correct repo for this site's comments. If you own this repo,
@@ -152,7 +159,8 @@ export async function assertOrigin() {
     to include <code>${origin}</code> in the list of origins.<br/><br/>
     Suggested configuration:<br/>
     <pre><code>${JSON.stringify({ origins: [origin] }, null, 2)}</code></pre>
-  </div>`);
-  scheduleMeasure();
-  throw new Error('Origin not permitted.');
+  </div>`
+  )
+  scheduleMeasure()
+  throw new Error('Origin not permitted.')
 }
