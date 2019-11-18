f30dc6a7e22ed55af54ad5f27d522addd69cc289
Gråborg <mikael.graborg@seb.se>
2019-11-18 17:37:58 +0100

    chore: add more github styling

--- src/issue-component.ts
+++ src/issue-component.ts
@@ -36,7 +36,7 @@ export class IssueComponent {
     this.element.innerHTML = `
     <div id="issue_1042" style="display: flex; flex-direction: row; margin-left: 16px;" >
       
-      <div id="issue-icon" style="margin-top: 3px;" >
+      <div id="issue-icon" style="margin-top: 5px;" >
         <span aria-label="Open issue">
           <svg viewBox="0 0 14 16" version="1.1" width="14" height="16" aria-hidden="true">
           <path fill-rule="evenodd" d="M7 2.3c3.14 0 5.7 2.56 5.7 5.7s-2.56 5.7-5.7 5.7A5.71 5.71 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zM7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm1 3H6v5h2V4zm0 6H6v2h2v-2z"></path></svg>
